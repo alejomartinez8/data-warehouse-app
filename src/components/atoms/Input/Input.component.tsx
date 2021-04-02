12488717deebcca50f87f7cda194cec2f3d52ca8
Alejandro Martinez <alejomartinez8@gmail.com>
2021-04-02 09:34:17 -0500

    Fix linter warning

--- src/components/atoms/Input/Input.component.tsx
+++ src/components/atoms/Input/Input.component.tsx
@@ -1,4 +1,3 @@
-import { HTMLAttributes } from 'react';
 import { StyledInput } from './Input.styled';
 
-export const Input = ({ ...props }: HTMLAttributes<HTMLInputElement>) => <StyledInput {...props} />;
+export const Input = ({ ...props }) => <StyledInput {...props} />;
