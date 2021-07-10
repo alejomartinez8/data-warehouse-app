1740d7397b2476ccf16267df351998af426e5d84
Alejandro Martinez <alejomartinez8@gmail.com>
2021-07-10 09:03:20 -0500

    Delete heroku harcoded

--- src/utils/handleError/handleError.util.ts
+++ src/utils/handleError/handleError.util.ts
@@ -1,24 +1,12 @@
 import { AxiosError } from 'axios';
-import { GetServerSidePropsContext } from 'next';
 import Router from 'next/router';
 
-const isProd = process.env.NODE_ENV === 'production';
-
-export function unauthorizedHandle(error: AxiosError, ctx?: GetServerSidePropsContext) {
+export function unauthorizedHandle(error: AxiosError) {
   const { response } = error;
 
-  // Client side
-  if (response?.status === 401 && !ctx?.req) {
+  if (response?.status === 401) {
     Router.replace('/login');
     throw error;
-  }
-
-  // Server side
-  if (response?.status === 401 && ctx?.req) {
-    ctx.res.writeHead(302, {
-      Location: isProd ? 'https://data-warehouse-am.herokuapp.com/login' : 'http://localhost:3000',
-    });
-    ctx.res?.end();
   }
 
   return null;
