89d03b778ee91eae01c9847de7d2380de4a057c2
Alejandro Martinez <alejomartinez8@gmail.com>
2021-07-31 09:13:50 -0500

    Refactor handleError

--- src/utils/handleError/handleError.util.ts
+++ src/utils/handleError/handleError.util.ts
@@ -3,13 +3,7 @@ import Router from 'next/router';
 import Cookies from 'js-cookie';
 
 export function unauthorizedHandle(error: AxiosError) {
-  const { response } = error;
-
-  if (response?.status === 401) {
-    Cookies.remove('user');
-    Router.replace('/login');
-    throw error;
-  }
-
-  return null;
+  Cookies.remove('user');
+  Router.replace('/login');
+  throw error;
 }
